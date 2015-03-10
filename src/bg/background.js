2e97cb844783ea5df666bf810765f2ae6403e917
Kenn Breece <kjb085@gmail.com>
2015-03-10 17:33:20 -0400

    Modify the background file to be an intermediary between the content script and the browser action

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,52 +1,19 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+var selection = ''
 
-
-//example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//   	chrome.pageAction.show(sender.tab.id);
-//     sendResponse();
-//   });
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     chrome.tabs.executeScript(tab.id, {file: "js/test.js"});
-// });
-
-// chrome.browserAction.addListener("copy", function(e) {
-//           console.log("Copy event", e);
-//           var text = e.clipboardData.getData("text/plain");
-//           console.log("Copy event", text);
-//           $scope.snippetPastedText = text;
-//           var activeGroup = $('.activegroup');
-//           $scope.groupId = activeGroup.data("id");
-//           $scope.pasteSnippet();
-//           //focusHiddenArea();
-//           //e.preventDefault();
-//       });
-
-
-	function getContentFromClipboard() {
-    var result = '';
-    var sandbox = document.getElementById('sandbox');
-    sandbox.value = '';
-    sandbox.select();
-    if (document.execCommand('paste')) {
-        result = sandbox.value;
-        console.log('got value from sandbox: ' + result);
-    }
-    sandbox.value = '';
-    return result;
-	}
-
-	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-		console.log("Received Message");
-		if(request.clipboard){
-			sendResponse(getContentFromClipboard())
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+		if(message.selection != undefined){
+			console.log(message.selection)
+			selection = message.selection
+			console.log(selection)
 		}
-	})
+	}
+)
 
-
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if(request == 'getSelection'){
+		console.log("Received Message");
+		console.log(selection);
+		sendResponse(selection);
+	}
+});
