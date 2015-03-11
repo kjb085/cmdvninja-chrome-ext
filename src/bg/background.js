850136494ea95f245c4e9b1b65944942ebf50611
Kenn Breece <kjb085@gmail.com>
2015-03-11 15:30:37 -0400

    Create event listeners to create notifications in browser

--- src/bg/background.js
+++ src/bg/background.js
@@ -2,13 +2,12 @@
 var selection = ''
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-		if(message.selection != undefined){
-			console.log(message.selection)
-			selection = message.selection
-			console.log(selection)
-		}
+	if(message.selection != undefined){
+		console.log(message.selection)
+		selection = message.selection
+		console.log(selection)
 	}
-)
+})
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request == 'getSelection'){
@@ -19,10 +18,23 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-		if (request.method = 'getToken' && !!(localStorage['cmdv_token'])){
-			sendResponse({token: localStorage['cmdv_token']})
-		}
-		else{
-			sendResponse({error: true})
-		}
+	if (request.method == 'getToken' && !!(localStorage['cmdv_token'])){
+		sendResponse({token: localStorage['cmdv_token']})
+	}
+	else{
+		sendResponse({error: true})
+	}
+})
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if (request.method == 'successNotif'){
+		chrome.notifications.create('cmdv', {type: 'basic', title: "Success!", message: "Snippet successfully posted to CmdV Ninja", iconUrl: '../../icons/ninja-small.png', priority: 0}, function(){})
+	}
+})
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if (request.method == 'failureNotif'){
+		chrome.notifications.create('', {type: 'basic', title: "Failure", message: "Snippet failed to post to CmdV Ninja\nPlease check authentication and ", iconUrl: '../../icons/ninja-small.png', priority: 0}, function(){})
+	}
 })
