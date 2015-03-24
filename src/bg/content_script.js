2626ca21b060f9e14f62130fa2946f097c2ceb90
Kenn Breece <kjb085@gmail.com>
2015-03-24 14:34:00 -0400

    Comment out console.logs and fix indentation error

--- src/bg/content_script.js
+++ src/bg/content_script.js
@@ -7,7 +7,7 @@ window.selection = "";
 document.addEventListener('selectionchange', function(){
 
 	selection = window.getSelection().toString();
-	console.log(selection);
+	// console.log(selection);
 	chrome.runtime.sendMessage({selection: selection})
 
 });
@@ -32,35 +32,35 @@ document.addEventListener('keydown', function(event1) {
 	 onkeydown = onkeyup = function(event2) {
 	keys[event2.keyCode] = event2.type == 'keydown';
 
-	console.log(keys) // Leave for later testing later refactors to clean up below if statement
+	// console.log(keys) // Leave for later testing later refactors to clean up below if statement
 
 	if ( (keys[91] === true || keys[91] === false) && (keys[69] === true || keys[69] === false) && (keys[16] === true || keys[16] === false) ) {
 		  
-			event2.preventDefault();
+		event2.preventDefault();
 
-			var snippet = window.selection
+		var snippet = window.selection
 
-			if(snippet == ""){
-				throw new Error("No selection made")
-			}
-			else if (!(userId)){
-				chrome.runtime.sendMessage({method: 'unauthNotif'})
-				throw new Error("Unauthorized access")
-			}
+		if(snippet == ""){
+			throw new Error("No selection made")
+		}
+		else if (!(userId)){
+			chrome.runtime.sendMessage({method: 'unauthNotif'})
+			throw new Error("Unauthorized access")
+		}
 
-		  $.ajax({
-					type: 'POST',
-					url: 'http://cmdvninja.herokuapp.com/api/users/' + userId + '/snippets',
-					data: { content: snippet, user: userId },
-					success: function(response){
-						console.log('Successfully posted snippet!');
-						chrome.runtime.sendMessage({method: 'successNotif'})
-					},
-					failure: function(){
-						console.log('Snippet failed to post');
-						chrome.runtime.sendMessage({method: 'failureNotif'})
-					}
-				});
+	  $.ajax({
+				type: 'POST',
+				url: 'http://cmdvninja.herokuapp.com/api/users/' + userId + '/snippets',
+				data: { content: snippet, user: userId },
+				success: function(response){
+					console.log('Successfully posted snippet!');
+					chrome.runtime.sendMessage({method: 'successNotif'})
+				},
+				failure: function(){
+					console.log('Snippet failed to post');
+					chrome.runtime.sendMessage({method: 'failureNotif'})
+				}
+			});
 		};
 	};
 });
